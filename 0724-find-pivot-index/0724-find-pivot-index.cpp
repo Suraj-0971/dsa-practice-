dc8b0e7f912f3c43f230feb4c56759eaa93d4ead
Suraj Jagtap <sr7freestyler@gmail.com>
2026-08-29 23:13:53 +0530

    Time: 0 ms (100%), Space: 35.7 MB (79.14%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int size = nums.size();
-        vector<int> suffix(size,0); // to store sum strictly after index i
-        vector<int> prefix(size,0); // to store sum strictly before index i
-        prefix[0] = 0; 
-        suffix[size - 1] = 0;
-        int pivot ; // to store the pivot index
-        bool flag = false; 
-        for(int i = 1, j = size-2 ; i < size, j >= 0; i++, j-- ){ // to calculate prefix sum and suffix sum
-            prefix[i] = prefix[i-1] + nums[i-1];
-            suffix[j] = suffix[j+1] + nums[j+1];
+        int array_sum = 0;
+        for(int i = 0 ; i < nums.size() ; i++){
+            array_sum += nums[i];
         }
-        for(int i = 0; i < size; i++){ // to iterate over prefix and suffix arrays and check for pivot index
-            if(prefix[i] == suffix[i]){
-                flag = true;
-                pivot = i ;
-                break;
+        int left = 0 , right = array_sum - left - nums[0];
+        if(left == right) return 0;
+        for(int i = 1; i < nums.size(); i++){
+            left += nums[i-1];
+            right = array_sum - left - nums[i];
+            if(left == right){
+                return i;
             }
         }
-        if(flag == false) return -1; // if no such index exists return -1
-        else return pivot; 
+        return -1;
     }
 };
